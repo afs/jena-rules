84e1e053944c086eadd008f4625a9c8d89aed634
Andy Seaborne <andy@apache.org>
2026-09-02 17:22:57 +0100

    rulestests

--- src/test/java/org/seaborne/jena/srl/junit/rulestests.java
+++ src/test/java/org/seaborne/jena/srl/junit/rulestests.java
@@ -21,22 +21,59 @@
 
 package org.seaborne.jena.srl.junit;
 
+import java.io.PrintStream;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.apache.jena.arq.junit.EarlReport;
 import org.apache.jena.arq.junit.manifest.TestMakers;
+import org.apache.jena.arq.junit.riot.VocabLangRDF;
 import org.apache.jena.arq.junit.textrunner.TextTestRunner;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.riot.Lang;
+import org.apache.jena.riot.RDFDataMgr;
+import org.apache.jena.vocabulary.TestManifest;
 
 public class rulestests {
 
+
+
     // See rdftests
     public static void main(String[] args) {
-        if ( args.length == 0 )
-            args = new String[] {"src/test/files/manifest-rules.ttl"};
-        List<String> manifests = Arrays.asList(args);
+        final PrintStream earlOut = System.out;
         TestMakers.install(RuleTests::makeRuleTest);
-//        List<TestMaker> next = List.of(SparqlTests::makeSPARQLTest, RiotTests::makeRIOTTest, RiotTests::makeRIOTTest);
-//        TestMakers.set(next);
-        TextTestRunner.run(manifests);
+
+        List<String> manifests = Arrays.asList(args);
+        boolean createEarlReport = ( manifests.contains("--earl") || manifests.contains("-earl") );
+        if ( ! createEarlReport ) {
+            TextTestRunner.run(manifests);
+            System.exit(0);
+        }
+
+        // Make it mutable.
+        manifests = new ArrayList<>(manifests);
+        manifests.remove("--earl");
+        manifests.remove("-earl");
+
+        EarlReport earlReport = new EarlReport(null/*bnode*/);
+        TextTestRunner.run(earlReport, manifests);
+        earlOut.println();
+        earlOut.println("# SPARQL_RL EARL Report");
+        Model model = earlReport.getModel();
+
+        // Lang
+        model.setNsPrefix("rdft", VocabLangRDF.getURI()) ;
+        // SPARQL
+        model.setNsPrefix("dawg", TestManifest.getURI()) ;
+        RDFDataMgr.write(earlOut, model, Lang.TURTLE);
+
+//            // ---
+//            Model meta = metadata(earlReport);
+            // Write meta separately so it is easy to find and can be extracted.
+//            RDFDataMgr.write(earlOut, model, Lang.TURTLE);
+//            earlOut.println();
+//            RDFDataMgr.write(earlOut, meta, Lang.TURTLE);
     }
 }
+
